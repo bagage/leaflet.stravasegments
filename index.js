6183ed92945858608917fc4396c966a391f45ed8
Gautier Pelloux-Prayer <gautier+git@damsy.net>
2019-03-09 12:59:43 +0100

    fix: properly handle xhr error

--- index.js
+++ index.js
@@ -155,11 +155,11 @@ L.Control.StravaSegments = L.Control.extend({
             if ((xhr.status === 200 || xhr.status === 0) && xhr.responseText) {
                 cb(null, xhr.responseText);
             } else {
-                cb(L.StravaSegments.getError(xhr));
+                cb(L.Control.StravaSegments.getError(xhr));
             }
         };
         xhr.onerror = function () {
-            cb(L.StravaSegments.getError(xhr));
+            cb(L.Control.StravaSegments.getError(xhr));
         };
         try {
             xhr.send();
